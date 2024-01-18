44d1c401baccee192f29ae7742543f13e889566c
fabrikatoroleg <fabrikatoroleg@gmail.com>
2024-01-18 13:46:09 +0200

    hosted the frontend

--- 2.4_rest_mongo_db/backend/src/app.ts
+++ 2.4_rest_mongo_db/backend/src/app.ts
@@ -4,17 +4,15 @@ import path from 'path';
 const app = express();
 const port = 3005;
 
-// Вказати шлях до статичних файлів (frontend)
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, '..', '..', 'frontend')));
 
-// Обробник для відправлення index.html при запиті на корінь
+// Обробник для відправлення index.html при запиті 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+    // Вказати шлях до index.html
+    res.sendFile(path.join(__dirname, '..', '..', 'frontend', 'index.html'));
 });
 
-// Решта вашого коду Express...
-
-// Слухати на заданому порту
+// Слухаємо на заданому порту
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
