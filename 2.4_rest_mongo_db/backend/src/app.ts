7b42045731dcabe65d984eb30b234c2b7f4f89ae
fabrikatoroleg <fabrikatoroleg@gmail.com>
2024-01-19 14:06:40 +0200

    added the library CORS

--- 2.4_rest_mongo_db/backend/src/app.ts
+++ 2.4_rest_mongo_db/backend/src/app.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 
 const app = express();
 const port = 3005;
 
+// Використовуйте cors middleware, щоб дозволити звертання до сервера з іншого домену
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
+);
+
+// Статичні файли з frontend
 app.use(express.static(path.join(__dirname, '..', '..', 'frontend')));
 
 // Обробник для відправлення index.html при запиті 
