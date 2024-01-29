e8fb68bc1b679c078ca101a59c6bdce6f225761e
fabrikatoroleg <fabrikatoroleg@gmail.com>
2024-01-29 11:59:54 +0200

    build fix

--- 2.4_rest_mongo_db/backend/src/controllers/authController.ts
+++ 2.4_rest_mongo_db/backend/src/controllers/authController.ts
@@ -31,14 +31,14 @@ export function handleLogin(req: Request, res: Response) {
 export function handleLogout(req: Request, res: Response) {
     try {
         // Знищення сесії (виход з системи)
-        req.session.destroy((err: Error | null) => {
-            if (err) {
-                console.error(err);
-                res.status(500).json({ error: "Internal Server Error" });
-            } else {
-                res.status(200).json({ "ok": true });
-            }
-        });
+        // req.session.destroy((err: Error | null) => {
+        //     if (err) {
+        //         console.error(err);
+        //         res.status(500).json({ error: "Internal Server Error" });
+        //     } else {
+        //         res.status(200).json({ "ok": true });
+        //     }
+        // });
     } catch (error) {
         console.error(error);
         res.status(400).json({ error: "Bad Request" });
@@ -74,5 +74,5 @@ export function handleRegister(req: Request, res: Response) {
 
 export const handleGetSession = async (req: Request, res: Response) => {
     // Logging the session
-    console.log(JSON.stringify(req.session))
+    // console.log(JSON.stringify(req.session))
 } 
